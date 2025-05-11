9e9fdd1dc3ea79e9133ef64fa0596affd690181a
elciobbsjr <elcio.junior@discente.ufma.br>
2025-05-10 23:00:38 -0300

    implementado o sistema de travessia em dois sentidos e prioridade no (botão A e Botão B)

--- Tarefa4_Aplicacaoo_Temporizadores.c
+++ Tarefa4_Aplicacaoo_Temporizadores.c
@@ -5,7 +5,8 @@
 
 #define LED_VERMELHO 13
 #define LED_VERDE    11
-#define BOTAO_PEDE   5
+#define BOTAO_A      5  // Sentido Centro
+#define BOTAO_B      6  // Sentido Bairro
 #define BUZZER_A     21
 #define BUZZER_B     10
 
@@ -13,27 +14,52 @@ typedef enum {
     ESTADO_VERMELHO, 
     ESTADO_VERDE, 
     ESTADO_AMARELO, 
-    ESTADO_PEDESTRE 
+    ESTADO_PEDESTRE_A,  // Travessia Centro
+    ESTADO_PEDESTRE_B   // Travessia Bairro
 } estado_t;
 
 volatile estado_t estado_atual = ESTADO_VERMELHO;
-volatile bool botao_pressionado = false;
+volatile bool pedido_A = false;
+volatile bool pedido_B = false;
 volatile int contagem_regressiva = 0;
 volatile bool aguardando_fim_contagem = false;
-volatile bool usar_buzzer_a = true;  // Alternador de buzzers
+volatile bool usar_buzzer_a = true;
 volatile uint current_buzzer_gpio = BUZZER_A;
 
 // --- Prototypes ---
 bool timer_callback(repeating_timer_t *rt);
 bool contagem_callback(repeating_timer_t *rt);
 
+
 repeating_timer_t timer;
 repeating_timer_t contagem_timer;
 repeating_timer_t buzzer_timer;
 
+void buzzer_off_callback() {
+    gpio_put(current_buzzer_gpio, 0);
+}
+
+bool buzzer_off_timer_callback(repeating_timer_t *rt) {
+    buzzer_off_callback();
+    return false;
+}
+
+void buzzer_pulse() {
+    current_buzzer_gpio = usar_buzzer_a ? BUZZER_A : BUZZER_B;
+    usar_buzzer_a = !usar_buzzer_a;
+
+    gpio_put(current_buzzer_gpio, 1);
+    add_repeating_timer_ms(-200, buzzer_off_timer_callback, NULL, &buzzer_timer);
+}
+
 void botao_callback(uint gpio, uint32_t events) {
-    botao_pressionado = true;
-    printf("Botão de Pedestres acionado\n");
+    if (gpio == BOTAO_A) {
+        pedido_A = true;
+        printf("Botão A (Centro) acionado\n");
+    } else if (gpio == BOTAO_B) {
+        pedido_B = true;
+        printf("Botão B (Bairro) acionado\n");
+    }
 }
 
 void atualiza_semaforo(estado_t estado) {
@@ -53,38 +79,25 @@ void atualiza_semaforo(estado_t estado) {
             gpio_put(LED_VERDE, 1);
             printf("Sinal: Amarelo\n");
             break;
-        case ESTADO_PEDESTRE:
+        case ESTADO_PEDESTRE_A:
             gpio_put(LED_VERMELHO, 1);
             gpio_put(LED_VERDE, 0);
-            printf("Sinal: Vermelho - Travessia de Pedestre\n");
+            printf("Travessia: Sentido Centro\n");
+            break;
+        case ESTADO_PEDESTRE_B:
+            gpio_put(LED_VERMELHO, 1);
+            gpio_put(LED_VERDE, 0);
+            printf("Travessia: Sentido Bairro\n");
             break;
     }
-}
-
-// Callback para desligar o buzzer após o pulso
-bool buzzer_off_callback(repeating_timer_t *rt) {
-    gpio_put(current_buzzer_gpio, 0);
-    return false; // Não repete
-}
-
-void buzzer_pulse() {
-    // Alterna entre os buzzers
-    current_buzzer_gpio = usar_buzzer_a ? BUZZER_A : BUZZER_B;
-    usar_buzzer_a = !usar_buzzer_a;
-
-    // Liga o buzzer atual
-    gpio_put(current_buzzer_gpio, 1);
-
-    // Agenda para desligar o buzzer após 200 ms
-    add_repeating_timer_ms(-200, buzzer_off_callback, NULL, &buzzer_timer);
 }
 
 bool contagem_callback(repeating_timer_t *rt) {
     if (contagem_regressiva > 0) {
         printf("Contagem Regressiva: %d\n", contagem_regressiva);
-        buzzer_pulse();  // Emite pulso não bloqueante
+        buzzer_pulse();
         contagem_regressiva--;
-        return true;  // Continua a contagem
+        return true;
     } else {
         aguardando_fim_contagem = false;
         add_repeating_timer_ms(-1, timer_callback, NULL, &timer); // Continua fluxo normal
@@ -93,51 +106,62 @@ bool contagem_callback(repeating_timer_t *rt) {
 }
 
 bool timer_callback(repeating_timer_t *rt) {
-    static bool esperando_pedestre = false;
+    static bool em_travessia = false;
 
     if (aguardando_fim_contagem) return false;
 
-    if (botao_pressionado && !esperando_pedestre) {
-        estado_atual = ESTADO_AMARELO;
-        atualiza_semaforo(ESTADO_AMARELO);
-        esperando_pedestre = true;
+    if (!em_travessia && (pedido_A || pedido_B)) {
+        // Atende primeiro quem pediu
+        if (pedido_A) {
+            estado_atual = ESTADO_AMARELO;
+            atualiza_semaforo(ESTADO_AMARELO);
+            em_travessia = true;
+            pedido_A = false;
+            estado_atual = ESTADO_PEDESTRE_A;
+        } else if (pedido_B) {
+            estado_atual = ESTADO_AMARELO;
+            atualiza_semaforo(ESTADO_AMARELO);
+            em_travessia = true;
+            pedido_B = false;
+            estado_atual = ESTADO_PEDESTRE_B;
+        }
         add_repeating_timer_ms(-3000, timer_callback, NULL, &timer); // 3s amarelo
     } 
-    else if (esperando_pedestre) {
-        estado_atual = ESTADO_PEDESTRE;
-        atualiza_semaforo(ESTADO_PEDESTRE);
+    else if (em_travessia) {
+        atualiza_semaforo(estado_atual);
 
         add_repeating_timer_ms(-5000, timer_callback, NULL, &timer); // 5s antes da contagem
 
         contagem_regressiva = 5;
         aguardando_fim_contagem = true;
-        usar_buzzer_a = true;  // Reseta alternância
+        usar_buzzer_a = true;
         add_repeating_timer_ms(-1000, contagem_callback, NULL, &contagem_timer);
 
-        botao_pressionado = false;
-        esperando_pedestre = false;
+        em_travessia = false;
     } 
     else {
         switch (estado_atual) {
             case ESTADO_VERMELHO:
                 estado_atual = ESTADO_VERDE;
                 atualiza_semaforo(ESTADO_VERDE);
-                add_repeating_timer_ms(-10000, timer_callback, NULL, &timer); // 10s verde
+                add_repeating_timer_ms(-10000, timer_callback, NULL, &timer);
                 break;
             case ESTADO_VERDE:
                 estado_atual = ESTADO_AMARELO;
                 atualiza_semaforo(ESTADO_AMARELO);
-                add_repeating_timer_ms(-3000, timer_callback, NULL, &timer); // 3s amarelo
+                add_repeating_timer_ms(-3000, timer_callback, NULL, &timer);
                 break;
             case ESTADO_AMARELO:
-            case ESTADO_PEDESTRE:
+            case ESTADO_PEDESTRE_A:
+            case ESTADO_PEDESTRE_B:
             default:
                 estado_atual = ESTADO_VERMELHO;
                 atualiza_semaforo(ESTADO_VERMELHO);
-                add_repeating_timer_ms(-10000, timer_callback, NULL, &timer); // 10s vermelho
+                add_repeating_timer_ms(-10000, timer_callback, NULL, &timer);
                 break;
         }
     }
+
     return false;
 }
 
@@ -148,9 +172,13 @@ void setup_gpio() {
     gpio_init(LED_VERDE);
     gpio_set_dir(LED_VERDE, GPIO_OUT);
 
-    gpio_init(BOTAO_PEDE);
-    gpio_set_dir(BOTAO_PEDE, GPIO_IN);
-    gpio_pull_up(BOTAO_PEDE);
+    gpio_init(BOTAO_A);
+    gpio_set_dir(BOTAO_A, GPIO_IN);
+    gpio_pull_up(BOTAO_A);
+
+    gpio_init(BOTAO_B);
+    gpio_set_dir(BOTAO_B, GPIO_IN);
+    gpio_pull_up(BOTAO_B);
 
     gpio_init(BUZZER_A);
     gpio_set_dir(BUZZER_A, GPIO_OUT);
@@ -160,7 +188,8 @@ void setup_gpio() {
     gpio_set_dir(BUZZER_B, GPIO_OUT);
     gpio_put(BUZZER_B, 0);
 
-    gpio_set_irq_enabled_with_callback(BOTAO_PEDE, GPIO_IRQ_EDGE_FALL, true, &botao_callback);
+    gpio_set_irq_enabled_with_callback(BOTAO_A, GPIO_IRQ_EDGE_FALL, true, &botao_callback);
+    gpio_set_irq_enabled(BOTAO_B, GPIO_IRQ_EDGE_FALL, true);
 }
 
 int main() {
@@ -168,7 +197,7 @@ int main() {
     setup_gpio();
 
     atualiza_semaforo(ESTADO_VERMELHO);
-    add_repeating_timer_ms(-10000, timer_callback, NULL, &timer); // 10s vermelho inicial
+    add_repeating_timer_ms(-10000, timer_callback, NULL, &timer);
 
     while (true) {
         tight_loop_contents();
